569d69499553060dc0f968c899b22cd590e6dc0e
kefeh <kefehcollins@gmail.com>
2025-12-06 07:09:27 +0100

    fix center div bug

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
 
   return (
-    <div className="min-h-screen flex items-center justify-items-center bg-cyan-900 p-4">
+    <div className="min-h-screen min-w-screen flex justify-center items-center bg-cyan-900 p-4">
       <div className=" p-8 rounded-xl shadow-lg w-full max-w-md border border-gray-100">
         <h1 className="text-2xl font-bold mb-6">German Address Validator</h1>
         <form className="space-y-4" onSubmit={(e) => e.preventDefault()}>
